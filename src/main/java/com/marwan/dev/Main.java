c5af74443e46d8d26917e82f4ffc9f877d5f6d78
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-04-16 18:44:18 +0200

    modify employee class by adding @AutoValue

--- src/main/java/com/marwan/dev/Main.java
+++ src/main/java/com/marwan/dev/Main.java
@@ -5,7 +5,7 @@ import com.marwan.dev.model.Employee;
 public class Main {
 
   public static void main(String[] args) {
-    Employee employee1 = new Employee(90, "Mohamed", "Ayman");
+    Employee employee1 = Employee.create(90, "Mohamed", "Ayman");
 
     System.out.println(employee1);
   }
--- src/main/java/com/marwan/dev/model/Employee.java
+++ src/main/java/com/marwan/dev/model/Employee.java
@@ -1,48 +1,18 @@
 package com.marwan.dev.model;
 
-import java.util.Objects;
+import com.google.auto.value.AutoValue;
 
-public class Employee {
+@AutoValue
+public abstract class Employee {
 
-  private final int id;
-  private final String fName;
-  private final String lName;
+  abstract int getId();
+
+  abstract String getFName();
+
+  abstract String getLName();
+
   // Add fields like you want to
-
-  public Employee(int id, String fName, String lName) {
-    this.id = id;
-    this.fName = fName;
-    this.lName = lName;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getfName() {
-    return fName;
-  }
-
-  public String getlName() {
-    return lName;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (!(o instanceof Employee employee)) {
-      return false;
-    }
-    return getId() == employee.getId() && Objects.equals(getfName(), employee.getfName())
-        && Objects.equals(getlName(), employee.getlName());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(getId(), getfName(), getlName());
-  }
-
-  @Override
-  public String toString() {
-    return "Employee{id=" + id + ", fName='" + fName + "', lName='" + lName + "'}";
+  public static Employee create(int id, String fName, String lName) {
+    return new AutoValue_Employee(id, fName, lName);
   }
 }
